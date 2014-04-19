11132db7eaa2723d8692842de2dc54b7394c9753
jcwrequests@comcast.net <jcwrequests@comcast.net>
2014-04-18 20:03:13 -0400

    Added Test to demo issue with Ionic Zip and Tiny FS

---
+++ TinyFS.Encryption.Tests/ZipTests.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class ZipTests
+
+    Private _tinyFile = "./tiny.fs"
+    Private _zip = "./tiny.zip"
+    <TestInitialize>
+    Public Sub SET_UP()
+        Using tiny As New TinyFS.EmbeddedStorage(_tinyFile)
+            Dim fileInfo = tiny.CreateFile("TEST")
+            Dim value = "This is a test"
+            Dim buffer() As Byte = System.Text.ASCIIEncoding.ASCII.GetBytes(value.ToCharArray)
+            tiny.Write(fileInfo, buffer, 0, buffer.Length)
+        End Using
+    End Sub
+    <TestMethod()> Public Sub CREATE_ZIP_WITH_TINY_FS_FILE()
+        Using archive As New Ionic.Zip.ZipFile(fileName:=_zip)
+            archive.AddFile(_tinyFile, String.Empty)
+            archive.Save()
+        End Using
+
+    End Sub
+
+End Class
